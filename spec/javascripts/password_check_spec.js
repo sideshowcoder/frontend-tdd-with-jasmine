f374501f25d9937040f7b7c92449b1d81805431f
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-03 12:28:36 +0200

    added jasmine-jquer and jquery

--- spec/javascripts/password_check_spec.js
+++ spec/javascripts/password_check_spec.js
@@ -1,5 +1,7 @@
 describe('PasswordCheck', function(){
   it('disables the submit for empty passwords', function(){
+    loadFixtures('form.html')
+    var submit = $(':submit')
     expect(submit.attr('disabled')).toBeTruthy()
   })
 })
