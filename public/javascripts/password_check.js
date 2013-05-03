68c349d9fddbb56affc4f3649d7eea60168bc711
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-03 12:48:22 +0200

    implement change value handling

--- public/javascripts/password_check.js
+++ public/javascripts/password_check.js
@@ -1,6 +1,12 @@
 var PasswordCheck = function(field){
   this.submit = field.siblings(':submit')
+  this.field = field
   this.submit.attr('disabled', 'disabled')
+  this.field.change(function(){
+    if(this.field.val().length > 4){
+      this.submit.removeAttr('disabled')
+    }
+  }.bind(this))
 }
 
 $.fn.passwordCheck = function(){
