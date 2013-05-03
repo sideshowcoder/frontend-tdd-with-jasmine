a663957167e005c30ca8ccd5a97a3ee022127c47
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-03 12:33:43 +0200

    call for passwordCheck to be defined

--- spec/javascripts/password_check_spec.js
+++ spec/javascripts/password_check_spec.js
@@ -2,6 +2,7 @@ describe('PasswordCheck', function(){
   it('disables the submit for empty passwords', function(){
     loadFixtures('form.html')
     var submit = $(':submit')
+    $('.password-field').passwordCheck()
     expect(submit.attr('disabled')).toBeTruthy()
   })
 })
