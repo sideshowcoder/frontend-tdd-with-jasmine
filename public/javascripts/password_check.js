a46071528767a4301db6e5963acd6022bc866b73
Philipp Fehre <philipp.fehre@googlemail.com>
2013-05-03 12:55:01 +0200

    refactor code to better factored for change

--- public/javascripts/password_check.js
+++ public/javascripts/password_check.js
@@ -2,11 +2,17 @@ var PasswordCheck = function(field){
   this.submit = field.siblings(':submit')
   this.field = field
   this.submit.attr('disabled', 'disabled')
-  this.field.change(function(){
-    if(this.field.val().length > 4){
-      this.submit.removeAttr('disabled')
-    }
-  }.bind(this))
+  this.field.change(this.handleNewValue.bind(this))
+}
+
+PasswordCheck.prototype.handleNewValue = function(){
+  if(this.validate()){
+    this.submit.removeAttr('disabled')
+  }
+}
+
+PasswordCheck.prototype.validate = function(){
+  return this.field.val().length > 4
 }
 
 $.fn.passwordCheck = function(){
